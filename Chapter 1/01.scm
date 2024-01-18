0b100170f007fdb2a9778c5d9e5fa34e33678f31
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-18 17:06:41 -0300

    refactor exercise 1.1

--- Chapter 1/01.scm
+++ Chapter 1/01.scm
@@ -33,15 +33,11 @@
 
 ;;Value: #f
 
-(if (and (> b a) (< b (* a b)))
-    b
-    a)
+(if (and (> b a) (< b (* a b))) b a)
 
 ;;Value: 4
 
-(cond ((= a 4) 6)
-      ((= b 4) (+ 6 7 a))
-      (else 25))
+(cond ((= a 4) 6) ((= b 4) (+ 6 7 a)) (else 25))
 
 ;;Value: 16
 
@@ -49,9 +45,6 @@
 
 ;;Value: 6
 
-(* (cond ((> a b) a)
-         ((< a b) b)
-         (else -1))
-   (+ a 1))
+(* (cond ((> a b) a) ((< a b) b) (else -1)) (+ a 1))
 
 ;;Value: 16
