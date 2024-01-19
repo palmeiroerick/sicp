2eb5ee07b232a9010983cdc5253e96a3a706cc40
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-19 15:54:03 -0300

    add description to exercise 1.3 and a alternative solution

--- Chapter-1/03.scm
+++ Chapter-1/03.scm
@@ -1,10 +1,22 @@
-(define (func a b c)
-  ;; (define (max x y) (if (or (> x y) (= x y)) x y))
-  
-  ;; (define (min x y) (if (or (< x y) (= x y)) x y))
-  
-  (define n1 (max a b))
-  
-  (define n2 (max (min a b) c))
-  
-  (+ (* n1 n1) (* n2 n2)))
+;; Exercise 1.3: Define a procedure that takes three numbers
+;; as arguments and returns the sum of the squares of the two
+;; larger numbers.
+
+;; Sum of Squares
+
+(define (sum-of-squares x y) (+ (* x x) (* y y)))
+
+;; Solution 1
+
+(define (sum-larger-squares1 a b c)
+  (define x (max a b))
+  (define y (max (min a b) c))
+  (sum-of-squares x y))
+
+;; Solution 2
+
+(define (sum-larger-squares2 a b c)
+  (cond 
+   ((and (<= a b) (<= a c)) (sum-of-squares b c))
+   ((and (<= b a) (<= b c)) (sum-of-squares a c))
+   (else (sum-of-squares a b))))
