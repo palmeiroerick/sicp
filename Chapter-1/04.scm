bf1d0284fbd16d04655dc44fbe34be2ab9655733
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-19 16:32:45 -0300

    adjust the length of the lines

--- Chapter-1/04.scm
+++ Chapter-1/04.scm
@@ -1,12 +1,11 @@
-;; Exercise 1.4: Observe that our model of evaluation allows
-;; for combinations whose operators are compound expres-
-;; sions. Use this observation to describe the behavior of the
-;; following procedure:
+;; Exercise 1.4: Observe that our model of evaluation allows for combinations
+;; whose operators are compound expressions. Use this observation to describe
+;; the behavior of the following procedure:
 
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
 
-;; As operators can be compound expressions, the if statement
-;; determines the operator based on the value of b:
+;; As operators can be compound expressions, the if statement determines the
+;; operator based on the value of b:
 ;; - If b > 0, the operator is +;
 ;; - If b < 0 or b = 0, the operator is -.
