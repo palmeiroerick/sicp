4057c494b5532565cebe2220f1e5f20838b0107b
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-02 18:04:43 -0300

    refactor leap-year exercise

--- Extras/leap-year.scm
+++ Extras/leap-year.scm
@@ -1,32 +1,11 @@
-;; Create a program that determine whether a given year is a leap year.
+;; Create a function that determines whether a given year is a leap year.
 
 (define (divisible-by? dividend divisor)
   (= (remainder dividend divisor) 0))
 
 (define (leap-year? year)
-  (cond
-   ((divisible-by? year 400) #t)
-   ((and (divisible-by? year 4)
-         (not (divisible-by? year 100))) #t)
-   (else #f)))
-
-(define (year-input)
-  (define year (read))
-  (if (and (number? year) (> year 0))
-      year
-      (begin
-        (display "Enter a valid value: ")
-        (year-input))))
-
-(newline)
-
-(display "Enter a year: ")
-(define year (year-input))
-
-(newline)
-
-(if (leap-year? year)
-    (display (string-append (number->string year) " is a leap year."))
-    (display (string-append (number->string year) " isn't a leap year.")))
-
-(newline)
+  (if (or (divisible-by? year 400)
+          (and (divisible-by? year 4)
+               (not (divisible-by? year 100))))
+      (string-append (number->string year) " is a leap year.")
+      (string-append (number->string year) " isn't a leap year.")))
